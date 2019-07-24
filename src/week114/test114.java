7552c0a5871d0c8665994127f613dac3a7864394
Kei Kogai <kogai@ece.ibaraki-ct.ac.jp>
2019-07-24 11:19:55 +0900

    Add files via upload

---
+++ src/week114/test114.java
@@ -0,0 +1,16 @@
+/* 自分の番号と名前をここに書く */
+import java.awt.*;
+import java.awt.event.*;
+
+class Counter implements ActionListener {
+    private Frame f1;
+    private Button plus, minus;
+    private TextField num;
+    public Counter() {
+    }
+    public void actionPerformed(ActionEvent e) {
+    }
+    public static void main(String[] args) {
+        Counter c = new Counter();
+    }
+}
